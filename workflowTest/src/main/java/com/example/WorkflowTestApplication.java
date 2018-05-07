12eb5590a27684df0e7da208218ea5480f10734c
novalabk <khkim@novalabk.com>
2018-05-07 13:14:34 +0900

    qqwewe

---
+++ workflowTest/src/main/java/com/example/WorkflowTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowTestApplication.class, args);
+	}ggg
+}
