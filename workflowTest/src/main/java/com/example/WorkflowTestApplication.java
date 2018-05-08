81be5dff9c52501b4bc7d9a045678711ba174dd3
novalabk <khkim@novalabk.com>
2018-05-08 15:45:34 +0900

    111

--- workflowTest/src/main/java/com/example/WorkflowTestApplication.java
+++ workflowTest/src/main/java/com/example/WorkflowTestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkflowTestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(WorkflowTestApplication.class, args);
 	}
 }
